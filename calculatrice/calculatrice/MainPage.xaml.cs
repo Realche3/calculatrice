a7b75a34ce54b97c07ce3bcd9f91b9339c3b2860
Realche3 <Realche333@gamil.com>
2021-04-13 09:34:38 -0300

    creation des button

--- calculatrice/calculatrice/MainPage.xaml.cs
+++ calculatrice/calculatrice/MainPage.xaml.cs
@@ -10,9 +10,32 @@ namespace calculatrice
 {
     public partial class MainPage : ContentPage
     {
+        #region Declaration
+
+        Button Plus, Minus, Divide, Multiply, Equal, Enter;
+
+        Button[] keypad;
+
+        #endregion
+
+        #region Init
+
+        private void InitComponent()
+        {
+            Plus = new Button() { Text = "Plus" };
+            Minus = new Button() { Text = "Minus" };
+            Divide = new Button() { Text = "Divide" };
+            Multiply = new Button() { Text = "Multiply" };
+            Enter = new Button() { Text = "Enter" };
+        }
         public MainPage()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region
+
+        #endregion
     }
 }
