0f3f641b5aa52bb161f7b7b552486a305e8306ac
Realche3 <Realche333@gamil.com>
2021-04-13 11:30:24 -0300

    Calculatrice Version 1.0

--- calculatrice/calculatrice/MainPage.xaml.cs
+++ calculatrice/calculatrice/MainPage.xaml.cs
@@ -13,29 +13,156 @@ namespace calculatrice
         #region Declaration
 
         Button Plus, Minus, Divide, Multiply, Equal, Enter;
+        Label resultLbl, bufferLbl;
 
         Button[] keypad;
 
-        #endregion
+        double buffer = 0.0;
+        double result = 0.0;
 
+        public double Buffer
+        {
+            get
+            {
+                return buffer;
+            }
+            set
+            {
+                buffer = value;
+                bufferLbl.Text = buffer.ToString();
+            }
+        }
+        public double Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                resultLbl.Text = result.ToString();
+            }
+        }
+        #endregion
+        
         #region Init
 
         private void InitComponent()
         {
+            resultLbl = new Label();
+            bufferLbl = new Label();
             Plus = new Button() { Text = "Plus" };
             Minus = new Button() { Text = "Minus" };
             Divide = new Button() { Text = "Divide" };
             Multiply = new Button() { Text = "Multiply" };
             Enter = new Button() { Text = "Enter" };
+
+            resultLbl.VerticalTextAlignment = TextAlignment.Center;
+            resultLbl.HorizontalTextAlignment = TextAlignment.Center;
+            resultLbl.HorizontalOptions = new LayoutOptions(LayoutAlignment.Fill, true);
+            resultLbl.VerticalOptions = new LayoutOptions(LayoutAlignment.Fill, true);
+            resultLbl.FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+
+            bufferLbl.VerticalTextAlignment = TextAlignment.Center;
+            bufferLbl.HorizontalTextAlignment = TextAlignment.Center;
+            bufferLbl.HorizontalOptions = new LayoutOptions(LayoutAlignment.Fill, true);
+            bufferLbl.VerticalOptions = new LayoutOptions(LayoutAlignment.Fill, true);
+
+            Plus.Pressed += Plus_Pressed;
+            Minus.Pressed += Minus_Pressed;
+            Divide.Pressed += Divide_Pressed;
+            Multiply.Pressed += Multiply_Pressed;
+            Enter.Pressed += Enter_Pressed;
+
+            //KeyPad Buttons
+            keypad = new Button[10];
+
+            for( var index = 0; index < keypad.Count(); ++ index)
+            {
+                ref var button = ref keypad[index];
+                button = new Button() { Text = index.ToString() };
+                int i = index;
+                button.Pressed += (o, e) => { KeypadButton_Pressed(i); };
+            }
+
+            // grid
+            int[] index_remap = new int[] { 0, 3, 2, 1, 6, 5, 4, 9, 8, 7 };
+            var grid = new rMultiplatform.AutoGrid();
+
+            grid.DefineGrid(3, 6);
+
+            grid.AutoAdd(bufferLbl);
+            grid.AutoAdd(resultLbl,2);
+
+            for (var index = keypad.Count()-1; index >=0; --index)
+            {                
+                ref var button = ref keypad[index_remap[index]];
+                if (index != 0)
+                    grid.AutoAdd(button);
+                else
+                    grid.AutoAdd(button, 2);
+            }
+
+            // setUp the buttons
+            grid.AutoAdd(Plus);
+            grid.AutoAdd(Minus);
+            grid.AutoAdd(Divide);
+            grid.AutoAdd(Multiply);
+           // grid.AutoAdd(Enter);
+            //afficher la grid
+
+            Content = grid;
+
         }
+
+
         public MainPage()
         {
             InitializeComponent();
+
+            InitComponent();
         }
         #endregion
 
-        #region
+        #region Operation Button
 
+        private void KeypadButton_Pressed(int index)
+        {
+            Buffer *= 10.0;
+            Buffer += (double)index;
+
+           // double value = index;
+        }
+
+        private void Enter_Pressed(object sender, EventArgs e)
+        {
+            Result = buffer;
+        }
+
+        private void Multiply_Pressed(object sender, EventArgs e)
+        {
+            Result *= buffer;
+            Buffer = 0.0;
+        }
+
+        private void Divide_Pressed(object sender, EventArgs e)
+        {
+            Result /= buffer;
+            Buffer = 0.0;
+        }
+
+        private void Minus_Pressed(object sender, EventArgs e)
+        {
+            Result -= buffer;
+            Buffer = 0.0;
+        }
+
+        private void Plus_Pressed(object sender, EventArgs e)
+        {
+            Result += buffer;
+            Buffer = 0.0;
+        }
         #endregion
     }
 }
